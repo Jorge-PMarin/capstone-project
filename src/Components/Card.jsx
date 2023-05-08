064274aaf228cfc7a6f01d9dbbbc087b89bfbb6c
jorge luis puerta marin <jorgeluis.sk8@hotmail.com>
2023-05-08 00:29:06 -0500

    card component finished

---
+++ src/Components/Card.jsx
@@ -0,0 +1,18 @@
+function Card({ name, imageUrl, description, price }) {
+  return (
+    <article className="card">
+      <img src={imageUrl} alt="" className="card__img" />
+      <div className="card__body">
+        <div className="card__heading__container">
+          <h2 className="card__heading">{name}</h2>
+          <span className="card__price__top">{price}</span>
+        </div>
+
+        <p className="card__description">{description}</p>
+        <span className="card__price__bottom">{price}</span>
+      </div>
+    </article>
+  );
+}
+
+export default Card;
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,53 @@
-import Hero from "./Hero";
+import Hero from './Hero';
+import Card from './Card';
 
-function HomePage () {
-    return (
-        <main>
-            <Hero />
-        </main>
-    )
+const dishes = [
+  {
+    name: 'Greek salad',
+    imageUrl:
+      'https://images.unsplash.com/photo-1599021419847-d8a7a6aba5b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=679&q=80',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, a facilis perferendis voluptatibus minus culpa quo minima quasi aspernatur deserunt incidunt deleniti, laudantium iusto dolor qui distinctio, perspiciatis quibusdam nobis.',
+    price: '$12.99',
+    id: 1
+  },
+  {
+    name: 'Bruchetta',
+    imageUrl:
+      'https://images.unsplash.com/photo-1594978583693-8dfdfc93f052?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, a facilis perferendis voluptatibus minus culpa quo minima quasi aspernatur deserunt incidunt deleniti, laudantium iusto dolor qui distinctio, perspiciatis quibusdam nobis.',
+    price: '$19.00',
+    id: 2
+  },
+  {
+    name: 'Lemon Dessert',
+    imageUrl:
+      'https://images.unsplash.com/photo-1528252941458-c1d19f902318?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, a facilis perferendis voluptatibus minus culpa quo minima quasi aspernatur deserunt incidunt deleniti, laudantium iusto dolor qui distinctio, perspiciatis quibusdam nobis.',
+    price: '$10.99',
+    id: 3
+  },
+];
+
+function HomePage() {
+  return (
+    <main className="homepage">
+      <Hero />
+      <section className="homepage__cards">
+        {dishes.map((dish) => (
+          <Card
+            name={dish.name}
+            imageUrl={dish.imageUrl}
+            description={dish.description}
+            price={dish.price}
+            key={dish.id}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default HomePage;
